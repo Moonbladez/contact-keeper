975a0672f90de310c0380a4975e46b8342d0dfad
Bekki Young <moonbladez@hotmail.com>
2020-06-18 13:05:20 +0200

    added navbar and route setup

---
+++ client/src/components/layout/Navbar.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import PropTypes from "prop-types";
+
+export const Navbar = ({ title, icon }) => {
+	return (
+		<div className='navbar bg-primary'>
+			<h1 className=''>
+				{icon} {title}
+			</h1>
+			<ul>
+				<li>
+					<NavLink to='/'>Home</NavLink>
+				</li>
+				<li>
+					<NavLink to='/about'>About</NavLink>
+				</li>
+			</ul>
+		</div>
+	);
+};
+
+Navbar.propTypes = {
+	title: PropTypes.string.isRequired,
+	icon: PropTypes.string,
+};
+
+Navbar.defaultProps = {
+	title: "Contact Keeper",
+};
