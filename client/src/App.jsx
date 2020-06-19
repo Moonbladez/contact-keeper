6a3dbb7f3bcb4022a1b938e1380468dba2b9cf0a
Bekki Young <moonbladez@hotmail.com>
2020-06-19 11:52:54 +0200

    auth context added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 import { ContactState } from "./context/contact/ContactState";
+import { AuthState } from "./context/auth/AuthState";
 
 import { Navbar } from "./components/layout/Navbar";
 import { Home } from "./components/pages/Home";
@@ -11,18 +12,20 @@ import "./App.css";
 
 export const App = () => {
 	return (
-		<ContactState>
-			<BrowserRouter>
-				<>
-					<Navbar />
-					<main className='container'>
-						<Switch>
-							<Route exact path='/' component={Home} />
-							<Route exact path='/about' component={About} />
-						</Switch>
-					</main>
-				</>
-			</BrowserRouter>
-		</ContactState>
+		<AuthState>
+			<ContactState>
+				<BrowserRouter>
+					<>
+						<Navbar />
+						<main className='container'>
+							<Switch>
+								<Route exact path='/' component={Home} />
+								<Route exact path='/about' component={About} />
+							</Switch>
+						</main>
+					</>
+				</BrowserRouter>
+			</ContactState>
+		</AuthState>
 	);
 };
