7f4c504375f91c4c26e38a1a0c91b68701d0fed6
Bekki Young <moonbladez@hotmail.com>
2020-06-21 21:21:46 +0200

    added login functionality

--- client/src/components/auth/Login.jsx
+++ client/src/components/auth/Login.jsx
@@ -1,6 +1,27 @@
-import React, { useState } from "react";
+import React, { useState, useContext, useEffect } from "react";
+import AuthContext from "../../context/auth/authContext";
+import AlertContext from "../../context/alert/alertContext";
 
-export const Login = () => {
+export const Login = (props) => {
+	const alertContext = useContext(AlertContext);
+	const authContext = useContext(AuthContext);
+
+	const { setAlert } = alertContext;
+	const { login, error, clearErrors, isAuthenticated } = authContext;
+
+	useEffect(() => {
+		if (isAuthenticated) {
+			props.history.push("/");
+		}
+
+		if (error == "Invalid Credentials") {
+			setAlert(error, "danger");
+			clearErrors();
+		}
+
+		// es-lint-disable-next-line
+	});
+
 	const [user, setUser] = useState({
 		email: "",
 		password: "",
@@ -14,7 +35,14 @@ export const Login = () => {
 
 	const onSubmit = (event) => {
 		event.preventDefault();
-		console.log("logged in");
+		if (email === "" || password === "") {
+			setAlert("Please fill in all fields", "danger");
+		} else {
+			login({
+				email,
+				password,
+			});
+		}
 	};
 
 	return (
@@ -25,7 +53,13 @@ export const Login = () => {
 			<form onSubmit={onSubmit}>
 				<div className='form-group'>
 					<label htmlFor='email'>email:</label>
-					<input type='email' name='email' value={email} onChange={onChange} />
+					<input
+						type='email'
+						name='email'
+						value={email}
+						onChange={onChange}
+						required
+					/>
 				</div>
 				<div className='form-group'>
 					<label htmlFor='password'>password:</label>
@@ -34,6 +68,7 @@ export const Login = () => {
 						name='password'
 						value={password}
 						onChange={onChange}
+						required
 					/>
 				</div>
 				<button type='submit' className='btn btn-primary btn-block'>
